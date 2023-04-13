53023a7b0cfef9b2fe6078d9e1ef16cfbd6b63bc
davi <davi.sdk@hotmail.com>
2023-04-12 21:31:41 -0300

    commit

---
+++ bootcamp-tqi-kotlin/desafios-codigo/desafios-codigo/src/LojaGeek.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LojaGeek {
+  
+  public static void main(String[] args) {
+
+    System.out.println("Insira a média das avaliações de desempenho dos funcionários: ");
+
+    Scanner scanner = new Scanner(System.in);
+    double media = scanner.nextDouble();
+
+    if (media < 5.0) {
+        System.out.println("REP");
+    } else if (media < 7.0) {
+        System.out.println("MED");
+    } else {
+        System.out.println("APR");
+    }
+    scanner.close();
+  }
+}
