3257019bebc88606d4d003552eb5d54d823f3878
davi <davi.sdk@hotmail.com>
2023-04-12 21:29:19 -0300

    commit

--- bootcamp-tqi-kotlin/desafios-codigo/desafios-codigo/src/MudancaparaIrlanda.java
+++ bootcamp-tqi-kotlin/desafios-codigo/desafios-codigo/src/MudancaparaIrlanda.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 
 public class MudancaparaIrlanda {
 
-  public static void main(String[] args) {
-    
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
 
-    System.out.println("Digite o Salario Bruto:");
-    double salarioBruto = input.nextDouble();
-    System.out.println("Digite os Beneficios:");
-    double beneficios = input.nextDouble();
+        Scanner input = new Scanner(System.in);
 
-    double imposto = 0;
-    
-    if (salarioBruto <= 1100.00) {
-        imposto = salarioBruto * 0.05;
-    } else if (salarioBruto <= 2500.00) {
-        imposto = salarioBruto * 0.1;
-    } else {
-        imposto = salarioBruto * 0.15;
+        try {
+            System.out.println("Digite o Salario Bruto:");
+            double salarioBruto = input.nextDouble();
+            System.out.println("Digite os Beneficios:");
+            double beneficios = input.nextDouble();
+
+            double imposto = 0;
+
+            if (salarioBruto <= 1100.00) {
+                imposto = salarioBruto * 0.05;
+            } else if (salarioBruto <= 2500.00) {
+                imposto = salarioBruto * 0.1;
+            } else {
+                imposto = salarioBruto * 0.15;
+            }
+
+            double salarioLiquido = salarioBruto - imposto + beneficios;
+
+            System.out.printf("Salario liquido: %.2f", salarioLiquido);
+        } finally {
+            input.close();
+        }
     }
-  
-    double salarioLiquido = salarioBruto - imposto + beneficios;
-    
-    System.out.printf("Salario liquido: %.2f", salarioLiquido);
 
-  }
 }
-
