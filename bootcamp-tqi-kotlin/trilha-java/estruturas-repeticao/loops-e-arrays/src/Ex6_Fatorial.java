a5029387cb3cb85b1a58870b50347db430a4daf2
davi <davi.sdk@hotmail.com>
2023-04-29 17:06:23 -0300

    commit

--- bootcamp-tqi-kotlin/trilha-java/estruturas-repeticao/loops-e-arrays/src/Ex6_Fatorial.java
+++ bootcamp-tqi-kotlin/trilha-java/estruturas-repeticao/loops-e-arrays/src/Ex6_Fatorial.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Ex6_Fatorial {
 
+    /*
+     * Faça um programa que calcule o fatorial de um número inteiro fornecido pelo
+     * usuário.
+     * x.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+     */
+
     public static void main(String[] args) {
-        
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Fatorial: ");
@@ -11,15 +17,15 @@ public class Ex6_Fatorial {
 
         int multiplicacao = 1;
 
-        System.out.print(fatorial +"! = ");
-        for(int i = fatorial ; i >= 1 ; i --) {
+        System.out.print(fatorial + "! = ");
+        for (int i = fatorial; i >= 1; i--) {
             multiplicacao = multiplicacao * i;
         }
 
         System.out.println(multiplicacao);
 
         scan.close();
-     
+
     }
-    
+
 }
