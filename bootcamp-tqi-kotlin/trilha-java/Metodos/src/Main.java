39b5fb296e1b2f52ed1c7b394ef86749ec5bed9d
davi <davi.sdk@hotmail.com>
2023-05-02 22:00:11 -0300

    commit

---
+++ bootcamp-tqi-kotlin/trilha-java/Metodos/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Calculadora
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(5, 5);
+        Calculadora.subtracao(10, 5);
+        Calculadora.multiplicacao(200, 100);
+        Calculadora.divisao(10, 2);
+
+        System.out.println();
+
+        // Mensagem 
+        System.out.println("Exercicio mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        System.out.println();
+
+        // Empréstimo 
+        System.out.println("Exercicio empréstimo");
+        Emprestimo.calcular(10000, 2);
+        Emprestimo.calcular(50000, 3);
+        Emprestimo.calcular(15000, 5);
+    }
+}
