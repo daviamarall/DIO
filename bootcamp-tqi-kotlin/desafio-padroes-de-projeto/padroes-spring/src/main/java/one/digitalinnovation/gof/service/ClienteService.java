518e0a9e32b47a90ca308116c0d06dddd78650ec
tqi_damaral <davi.sdk@hotmail.com>
2023-06-08 22:09:26 -0300

    commit padroes-spring

--- bootcamp-tqi-kotlin/desafio-padroes-de-projeto/padroes-spring/src/main/java/one/digitalinnovation/gof/service/ClienteService.java
+++ bootcamp-tqi-kotlin/desafio-padroes-de-projeto/padroes-spring/src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -6,19 +6,19 @@ import one.digitalinnovation.gof.model.Cliente;
  * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
  * isso, se necessário, podemos ter multiplas implementações dessa mesma
  * interface.
- * 
+ *
  * @author falvojr
  */
 public interface ClienteService {
 
-	Iterable<Cliente> buscarTodos();
+    Iterable<Cliente> buscarTodos();
 
-	Cliente buscarPorId(Long id);
+    Cliente buscarPorId(Long id);
 
-	void inserir(Cliente cliente);
+    void inserir(Cliente cliente);
 
-	void atualizar(Long id, Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
 
-	void deletar(Long id);
+    void deletar(Long id);
 
 }
--- bootcamp-tqi-kotlin/desafio-padroes-de-projeto/padroes-spring/src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
+++ bootcamp-tqi-kotlin/desafio-padroes-de-projeto/padroes-spring/src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
@@ -9,15 +9,14 @@ import one.digitalinnovation.gof.model.Endereco;
 /**
  * Client HTTP, criado via <b>OpenFeign</b>, para o consumo da API do
  * <b>ViaCEP</b>.
- * 
+ *
+ * @author falvojr
  * @see <a href="https://spring.io/projects/spring-cloud-openfeign">Spring Cloud OpenFeign</a>
  * @see <a href="https://viacep.com.br">ViaCEP</a>
- * 
- * @author falvojr
  */
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
-	@GetMapping("/{cep}/json/")
-	Endereco consultarCep(@PathVariable("cep") String cep);
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
 }
